91806633923bcabe41e6255ad7f854c309d626e9
Nyan Linn Htet <nyanlinhtet2301@gmail.com>
2024-01-30 08:28:01 +0630

    Done: mulit select from and to

--- resources/views/admin/tracks/create.blade.php
+++ resources/views/admin/tracks/create.blade.php
@@ -4,7 +4,7 @@
 <main id="main" class="main">
 
     <div class="pagetitle">
-      
+
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="{{ route('admin.track.index') }}"><h1>{{ trans("cruds.track.title") }}</h1></a></li>
@@ -22,7 +22,7 @@
                 </div>
                 <div class="card-body p-2">
                     <div class="row">
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-12 mb-2">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-12 col-sm-12 mb-2">
                             <div class="form-group">
                                 <label class="required mb-2" for="from">{{ trans('global.from') }}</label>
                                 <select name="from" id="from" class="form-control {{ $errors->has('from') ? 'is-invalid' : '' }}">
@@ -38,7 +38,12 @@
                                 @endif
                             </div>
                         </div>
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-12 mb-2">
+                        <div class="col-xl-1 col-lg-1 col-md-1" style="margin-top: 2rem;">
+                            <button type="button" class="btn btn-sm btn-primary from-plus">
+                                <i class="fas fa-plus"></i>
+                            </button>
+                        </div>
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-12 col-sm-12 mb-2">
                             <div class="form-group">
                                 <label class="required mb-2" for="to">{{ trans('global.to') }}</label>
                                 <select name="to" id="to" class="form-control {{ $errors->has('to') ? 'is-invalid' : '' }}">
@@ -54,6 +59,15 @@
                                 @endif
                             </div>
                         </div>
+                        <div class="col-xl-1 col-lg-1 col-md-1" style="margin-top: 2rem;">
+                            <button type="button" class="btn btn-sm btn-primary to-plus">
+                                <i class="fas fa-plus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 from-append"></div>
+                        <div class="col-6 to-append"></div>
                     </div>
                     <div class="row">
                         <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-12 mb-2">
@@ -102,10 +116,69 @@
 @endsection
 @section('scripts')
 <script>
-$(document).ready(function() {
-    $('#others').select2({
-        width: '100%'
+    $(document).ready(function() {
+        $('#others').select2({
+            width: '100%'
+        });
     });
-});
+    var fromText = `<div class="row">
+                        <div class="col-xl-10 col-lg-10 col-md-10 col-10 col-sm-10 mb-2">
+                            <div class="form-group">
+                                <label class="required mb-2" for="from">{{ trans('global.from') }}</label>
+                                <select name="from" id="from" class="form-control {{ $errors->has('from') ? 'is-invalid' : '' }}">
+                                    <option value="" disabled selected>{{ trans('global.please_select') }}</option>
+                                    @foreach ($cities as $id => $name)
+                                        <option value="{{ $id }}"{{ old('from') ? 'selected' : '' }}>{{ $name }}</option>
+                                    @endforeach
+                                </select>
+                                @if($errors->has('from'))
+                                    <div class="invalid-feedback">
+                                        {{ $errors->first('from') }}
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-2" style="margin-top: 2rem;">
+                            <button type="button" class="btn btn-sm btn-danger from-minus">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>`;
+
+    var toText = `<div class="row">
+                    <div class="col-xl-10 col-lg-10 col-md-10 col-10 col-sm-10 mb-2">
+                            <div class="form-group">
+                                <label class="required mb-2" for="to">{{ trans('global.to') }}</label>
+                                <select name="to" id="to" class="form-control {{ $errors->has('to') ? 'is-invalid' : '' }}">
+                                    <option value="" disabled selected>{{ trans('global.please_select') }}</option>
+                                    @foreach ($cities as $id => $name)
+                                        <option value="{{ $id }}"{{ old('to') ? 'selected' : '' }}>{{ $name }}</option>
+                                    @endforeach
+                                </select>
+                                @if($errors->has('to'))
+                                    <div class="invalid-feedback">
+                                        {{ $errors->first('to') }}
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-2" style="margin-top: 2rem;">
+                            <button type="button" class="btn btn-sm btn-danger to-minus">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>`;
+    $('.from-plus').click(function() {
+        $('.from-append').append(fromText);
+    });
+    $('.to-plus').click(function() {
+        $('.to-append').append(toText);
+    });
+    $(document).on('click', '.from-minus', function() {
+        $(this).closest('.row').remove();
+    })
+    $(document).on('click', '.to-minus', function() {
+        $(this).closest('.row').remove();
+    });
 </script>
 @endsection
