b026f7ac6696bba14aeb5f1021fced987976ca75
Htet-Lin-Aung <htetlinaungtuinfo@gmail.com>
2024-05-05 14:15:54 +0630

    updated: google drive provider

--- app/Providers/GoogleDriveServiceProvider.php
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -4,11 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Storage;
-use Masbug\Flysystem\GoogleDrive\Client;
-use Masbug\Flysystem\GoogleDrive\Drive; 
-use Masbug\Flysystem\GoogleDrive\GoogleDriveAdapter; 
-use League\Flysystem\Filesystem; 
-use League\Flysystem\FilesystemAdapter; 
 
 class GoogleDriveServiceProvider extends ServiceProvider
 {
@@ -30,16 +25,17 @@ class GoogleDriveServiceProvider extends ServiceProvider
     public function boot()
     {
         Storage::extend('google', function ($app, $config) {
-            $client = new Client(); // Create a Client instance
+            $options = [];
+            $client = new \Google\Client();
             $client->setClientId($config['clientId']);
             $client->setClientSecret($config['clientSecret']);
             $client->refreshToken($config['refreshToken']);
 
-            $service = new Drive($client); // Create a Drive instance
-            $adapter = new GoogleDriveAdapter($service, $config['folder'] ?? '/', $options ?? []); // Use default options if not provided
+            $service = new \Google\Service\Drive($client);
+            $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
+            $driver = new \League\Flysystem\Filesystem($adapter);
 
-            $driver = new Filesystem($adapter); // Create a Filesystem instance
-            return new FilesystemAdapter($driver, $adapter); // Create a FilesystemAdapter instance
+            return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
         });
     }
 }
